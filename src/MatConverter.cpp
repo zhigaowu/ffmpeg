3450082c12d7ed755e3c6e58ef4ecd106438d3b9
Zhigao <wuzhigaoem@163.com>
2022-01-17 10:27:44 +0800

    refactor convert logic
    convert with two kinds of size:
    1, origin size
    2, target size

--- src/MatConverter.cpp
+++ src/MatConverter.cpp
@@ -53,27 +53,54 @@ namespace ffmpeg
                 real_frame = _device_mapper;
             }
 
-            if (!_sws_context)
-            {
-				_origin_size = cv::Size(frame->width, frame->height);
-                _sws_context = sws_getContext(frame->width, frame->height, (AVPixelFormat)real_frame->format, _target_size.width, _target_size.height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
-            }
-			else
+			if (!_target_size.empty())
 			{
-				if (_origin_size.width != frame->width || _origin_size.height != frame->height)
+				if (!_sws_context)
 				{
 					_origin_size = cv::Size(frame->width, frame->height);
-
-					sws_freeContext(_sws_context);
 					_sws_context = sws_getContext(frame->width, frame->height, (AVPixelFormat)real_frame->format, _target_size.width, _target_size.height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
 				}
-			}
+				else
+				{
+					if (_origin_size.width != frame->width || _origin_size.height != frame->height)
+					{
+						_origin_size = cv::Size(frame->width, frame->height);
 
-            // check image size and adjust it
-            if (image.cols != _target_size.width || image.rows != _target_size.height)
-            {
-                image = cv::Mat(_target_size, CV_8UC3);
-            }
+						sws_freeContext(_sws_context);
+						_sws_context = sws_getContext(frame->width, frame->height, (AVPixelFormat)real_frame->format, _target_size.width, _target_size.height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
+					}
+				}
+
+				// check image size and adjust it
+				if (image.size() != _target_size)
+				{
+					image = cv::Mat(_target_size, CV_8UC3);
+				}
+			} 
+			else
+			{
+				if (!_sws_context)
+				{
+					_origin_size = cv::Size(frame->width, frame->height);
+					_sws_context = sws_getContext(frame->width, frame->height, (AVPixelFormat)real_frame->format, _origin_size.width, _origin_size.height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
+				}
+				else
+				{
+					if (_origin_size.width != frame->width || _origin_size.height != frame->height)
+					{
+						_origin_size = cv::Size(frame->width, frame->height);
+
+						sws_freeContext(_sws_context);
+						_sws_context = sws_getContext(frame->width, frame->height, (AVPixelFormat)real_frame->format, _origin_size.width, _origin_size.height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
+					}
+				}
+
+				// check image size and adjust it
+				if (image.size() != _origin_size)
+				{
+					image = cv::Mat(_origin_size, CV_8UC3);
+				}
+			}
 
             // map resource
             if ((res = av_image_fill_arrays(_image_mapper->data, _image_mapper->linesize, image.data, AV_PIX_FMT_BGR24, image.cols, image.rows, 1)) < 0)
